5c4707a91e483b50683bf1f43ef25bf1efa8dca2
Katie <Katie.McGlawn@gmail.com>
2019-11-12 19:13:23 -0600

    SolvedSmallestNumber

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -5,22 +5,32 @@ namespace ChallengeSets
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            if (num1 == num2)
+            {
+                return true;
+            }
+            return false;
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            double difference = minuend - subtrahend;
+            return difference;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int sum = number1 + number2;
+            return sum;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 <= number2)
+            {
+                return number1;
+            }
+            return number2;
         }
 
         public long Multiply(long factor1, long factor2)
