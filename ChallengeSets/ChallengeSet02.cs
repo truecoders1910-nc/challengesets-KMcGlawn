7e1a941624017de67a9c4691481513bdcad4c103
Katie <Katie.McGlawn@gmail.com>
2019-12-06 14:49:24 -0600

    FirstChallengeSolved

--- ChallengeSets/ChallengeSet02.cs
+++ ChallengeSets/ChallengeSet02.cs
@@ -1,13 +1,234 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace ChallengeSets
 {
     public class ChallengeSet02
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            string letter = c.ToString();
+
+            if (letter == "A")
+            {
+                return true;
+            }
+            if (letter == "B")
+            {
+                return true;
+            }
+            if (letter == "C")
+            {
+                return true;
+            }
+            if (letter == "D")
+            {
+                return true;
+            }
+            if (letter == "E")
+            {
+                return true;
+            }
+            if (letter == "F")
+            {
+                return true;
+            }
+            if (letter == "G")
+            {
+                return true;
+            }
+            if (letter == "H")
+            {
+                return true;
+            }
+            if (letter == "I")
+            {
+                return true;
+            }
+            if (letter == "J")
+            {
+                return true;
+            }
+            if (letter == "K")
+            {
+                return true;
+            }
+            if (letter == "L")
+            {
+                return true;
+            }
+            if (letter == "M")
+            {
+                return true;
+            }
+            if (letter == "N")
+            {
+                return true;
+            }
+            if (letter == "O")
+            {
+                return true;
+            }
+            if (letter == "P")
+            {
+                return true;
+            }
+            if (letter == "Q")
+            {
+                return true;
+            }
+            if (letter == "R")
+            {
+                return true;
+            }
+            if (letter == "S")
+            {
+                return true;
+            }
+            if (letter == "T")
+            {
+                return true;
+            }
+            if (letter == "U")
+            {
+                return true;
+            }
+            if (letter == "V")
+            {
+                return true;
+            }
+            if (letter == "W")
+            {
+                return true;
+            }
+            if (letter == "X")
+            {
+                return true;
+            }
+            if (letter == "Y")
+            {
+                return true;
+            }
+            if (letter == "Z")
+            {
+                return true;
+            }
+
+            if (letter == " ")
+            {
+                return true;
+            }
+            if (letter == "a")
+            {
+                return true;
+            }
+            if (letter == "b")
+            {
+                return true;
+            }
+            if (letter == "c")
+            {
+                return true;
+            }
+            if (letter == "d")
+            {
+                return true;
+            }
+            if (letter == "e")
+            {
+                return true;
+            }
+            if (letter == "f")
+            {
+                return true;
+            }
+            if (letter == "g")
+            {
+                return true;
+            }
+            if (letter == "h")
+            {
+                return true;
+            }
+            if (letter == "i")
+            {
+                return true;
+            }
+            if (letter == "j")
+            {
+                return true;
+            }
+            if (letter == "k")
+            {
+                return true;
+            }
+            if (letter == "l")
+            {
+                return true;
+            }
+            if (letter == "m")
+            {
+                return true;
+            }
+            if (letter == "n")
+            {
+                return true;
+            }
+            if (letter == "o")
+            {
+                return true;
+            }
+            if (letter == "p")
+            {
+                return true;
+            }
+            if (letter == "q")
+            {
+                return true;
+            }
+            if (letter == "r")
+            {
+                return true;
+            }
+            if (letter == "s")
+            {
+                return true;
+            }
+            if (letter == "t")
+            {
+                return true;
+            }
+            if (letter == "u")
+            {
+                return true;
+            }
+            if (letter == "v")
+            {
+                return true;
+            }
+            if (letter == "w")
+            {
+                return true;
+            }
+            if (letter == "x")
+            {
+                return true;
+            }
+            if (letter == "y")
+            {
+                return true;
+            }
+            if (letter == "z")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+            
         }
 
         public bool CountOfElementsIsEven(string[] vals)
