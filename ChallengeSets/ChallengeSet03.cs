41e2160f49a4fb54db75fd6623296f93114c8dbd
Katie <Katie.McGlawn@gmail.com>
2019-12-19 19:18:05 -0600

    SolvedMinus

--- ChallengeSets/ChallengeSet03.cs
+++ ChallengeSets/ChallengeSet03.cs
@@ -64,7 +64,7 @@ namespace ChallengeSets
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+          return  nums.Last() - nums.First();
         }
         public int[] GetOddsBelow100()
         {
